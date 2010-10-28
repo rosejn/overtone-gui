2231747d713cd9f619f9669e90b3c3701508e90c
Jeff Rose <rosejn@gmail.com>
2010-10-28 12:24:36 +0200

    Dial handler works and now holding shift means finer adjustment.
    * Fixed a bug in updating the value so it works smoother now.
    * Added a shift modifier to decrease sensitivity for finer adjustments
     - normally each pixel up or down means 0.01 value change
     - with shift value change is 0.001

--- src/overtone/gui/dial.clj
+++ src/overtone/gui/dial.clj
@@ -28,26 +28,26 @@
                                  -130 -200 Arc2D/PIE)
          center (sg-shape)
          value (atom 0.8)
-         start-y (atom 0)]
+         last-y (atom 0)]
      (doto ring
-       (set-antialias! :on)       
+       (set-antialias! :on)
        (set-mode! :stroke)
        (set-draw-stroke! 1.5)
        (set-stroke-paint! (color :stroke-1))
        (set-shape! (Arc2D$Float. 0 0 SIZE SIZE -130 -280 Arc2D/OPEN)))
      (doto back-fill
-       (set-antialias! :on)       
+       (set-antialias! :on)
        (set-mode! :fill)
        (set-fill-paint! (color :fill-1))
        (set-shape! (Arc2D$Float. 0 0 SIZE SIZE
                                  -130 -280 Arc2D/PIE)))
      (doto front-fill
-       (set-antialias! :on)       
+       (set-antialias! :on)
        (set-mode! :fill)
        (set-fill-paint! (color :stroke-1))
        (set-shape! fill-arc))
      (doto center
-       (set-antialias! :on)       
+       (set-antialias! :on)
        (set-mode! :fill)
        (set-fill-paint! (color :background))
        (set-shape! (Arc2D$Float. (/ SIZE 4)
@@ -55,7 +55,7 @@
                                  (/ SIZE 2)
                                  (/ SIZE 2)
                                  0 360 Arc2D/OPEN)))
-     (add-watch value :gensym 
+     (add-watch value :gensym
         (fn [_ _ _ new-val]
           (let [angle (- (* new-val 280))]
             (doto fill-arc
@@ -67,14 +67,18 @@
      (let [press-handler
            (fn [event]
              (let [y (.getY event)]
-               (reset! start-y y)))
+               (reset! last-y y)))
 
            drag-handler
            (fn [event]
              (let [cur-y (.getY event)
-                   dy (- cur-y @start-y)
-                   val (max 0 (min (* 0.01 dy) 1))]
+                   dy (- @last-y cur-y)
+                   dv (if (.isShiftDown event)
+                        0.001
+                        0.01)
+                   val (max 0 (min (+ @value (* dv dy)) 1))]
                (reset! value val)
+               (reset! last-y cur-y)
                (if handler
                  (handler val))))]
        (on-mouse-pressed back-fill press-handler)
@@ -97,7 +101,8 @@
 (defn dial-panel []
   (let [p (sg-panel 400 400)
         group (sg-group)
-        background (sg-shape)]
+        background (sg-shape)
+        handler #(println %)]
     (doto background
       (set-mode! :fill)
       (set-fill-paint! (color :background))
@@ -105,7 +110,9 @@
     (.add group background)
 
     (dotimes [i 4]
-      (.add group (translate (:group (dial #(println %)) (+ 50 (* i 60)) 50)))
+      (.add group (translate (:group (dial handler)) 
+                             (+ 50 (* i 60)) 
+                             50)))
 
     (set-scene! p group)
     p))
